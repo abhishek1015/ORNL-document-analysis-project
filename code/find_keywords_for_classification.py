cbe872cb80ffea6fdff9fd5388308cb2578ec320
dubeyak <dubeyak@ornl.gov>
2018-12-13 18:54:30 -0500

    adding new file

---
+++ code/find_keywords_for_classification.py
@@ -0,0 +1,28 @@
+def find_keywords_for_classification(reports, idx, labellist):
+
+    num_of_reports = len(reports);
+    
+    for report in reports:
+        
+    reports[1]
+    
+    num_of_labels = len(labellist);
+    
+        
+    
+    
+    
+# Created by Abhishek K Dubey
+# Date: Dec 13, 2018
+
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 13 15:58:47 2018
+
+@author: 8kd
+"""
+
+
